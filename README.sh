548f2ba0a0b641837358dd49ac689f3c01299079
Ahmad Asaad <70626969+iAhmadGad@users.noreply.github.com>
2024-01-25 16:50:00 +0300

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,13 @@
+~ $ whoami
+Ahmad Asaad
+~ $ echo $INFO
+name: Ahmad
+age: 16
+birthday: 14 - 5
+grade: 11
+pronouns: He - him
+from: Egypt
+lives_in: Saudi Arabia
+hobbies: Coding, problem solving, solving maths homework
+spoken_languages: Arabic, English, a bit of German
+programming_languages: Java, Python, C, C++, Haskell
